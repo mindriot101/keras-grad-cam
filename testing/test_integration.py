ac1771330b9d1b498c6d3e2ea6e4481caa3948e5
Simon Walker <s.r.walker101@googlemail.com>
2019-07-26 23:20:47 +0100

    Use snapshot testing

--- testing/test_integration.py
+++ testing/test_integration.py
@@ -2,10 +2,12 @@ import sys
 sys.path.append(".")
 from gradcam import GradCam
 
-def test_everything():
+def test_everything(snapshot):
     filename = "examples/cat_dog.png"
     layer_name = "block5_conv3"
     model_filename = "model.h5"
 
     gc = GradCam.from_hdf(model_filename)
     cam, heatmap = gc.compute_cam(filename, layer_name)
+    snapshot.assert_match(cam)
+    snapshot.assert_match(heatmap)
